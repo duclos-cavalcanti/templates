daf09b0420d6271e5594a32123abfadbe3930038
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-11-30 17:45:10 -0500

    Update

--- pytest/main.py
+++ pytest/main.py
@@ -1,12 +1,39 @@
+import ipdb
 
 def inc(val):
     return val + 1
 
-def dec(val):
-    return val - 1
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ipdb.set_trace()
+        prev = -101
+        i = 0
+        for n in nums:
+            if n == prev:
+                del nums[i]
+            else:
+                prev = n
+            i = i + 1
+        return len(nums)
 
-def main():
-    print(inc(1))
 
 if __name__ == "__main__":
-    main()
+    s = Solution()
+
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    print("IN: \t[ ", end='')
+    for n in nums:
+        print(f"{n} ", end='')
+    print("]")
+
+    ret = s.removeDuplicates(nums)
+
+    print(f"RET:\t {ret}")
+    print("OUT: \t[ ", end='')
+    for n in nums:
+        print(f"{n} ", end='')
+    print("]")
--- pytest/tests/unit_test.py
+++ pytest/tests/unit_test.py
@@ -1,12 +1,6 @@
 import pytest
-import sys
-
-sys.path.append('src') # hacky
-
 import main
 
 def test_inc():
     assert 2 == main.inc(1)
 
-def test_inc_fail():
-    assert 2 == main.inc(2)
